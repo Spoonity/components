50d448db74a09fe6b831cfe686c8e6084e8b4d57
Gabriel <gabriel@spoonity.com>
2021-03-24 16:50:28 -0500

    sp data visualization added

---
+++ Components/src/app/Components/Organisms/data-visualization/data-visualization.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sp-data-visualization',
+  templateUrl: './data-visualization.component.html',
+  styleUrls: ['./data-visualization.component.less']
+})
+export class DataVisualizationComponent implements OnInit {
+
+ @Input() title: string;
+ @Input() description: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- Components/src/app/organisms/organisms.component.ts
+++ Components/src/app/organisms/organisms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType } from '../Components/Organisms/chart/chart.component';
 
 @Component({
   selector: 'app-organisms',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./organisms.component.less']
 })
 export class OrganismsComponent implements OnInit {
+  chartData = [
+    { data: [330, 600, 260, 700], label: 'Account A' },
+    { data: [120, 455, 100, 340], label: 'Account B' }
+  ];
 
+  chartLabels = ['January', 'February', 'Mars', 'April'];
+  type1:ChartType = ChartType.line;
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
