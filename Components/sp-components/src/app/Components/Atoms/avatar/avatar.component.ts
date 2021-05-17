f133c92e0a6485eb4865ea00002d4da49453dad0
Gabriel <gabriel@spoonity.com>
2021-05-17 11:51:06 -0500

    Storybook added

--- Components/sp-components/src/app/Components/Atoms/avatar/avatar.component.ts
+++ Components/sp-components/src/app/Components/Atoms/avatar/avatar.component.ts
@@ -8,14 +8,17 @@ import { AvatarSize } from '../../../utils/enums';
 })
 export class AvatarComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
   @Input() size: AvatarSize;
   @Input() text: string;
   @Input() color: string;
   @Input() backgroundColor: string;
+
+  styles: { 'background-color': string; color: string; };
+
+  constructor() { }
+
+  ngOnInit() {
+    this.styles = { 'background-color': this.backgroundColor, 'color': this.color };
+  }
 
 }
