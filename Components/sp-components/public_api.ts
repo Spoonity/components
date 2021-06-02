7c8f7a717b441b59c0c8f94b36765eb8b34f1c00
Gabriel Rodriguez <gabriel@spoonity.com>
2021-06-02 14:34:23 -0500

    sp-omponents exports added

--- Components/sp-components/public_api.ts
+++ Components/sp-components/public_api.ts
@@ -1,3 +1,5 @@
 export * from './src/app/sp-components.module';
 export * from './src/app/sp-component/sp-components.component';
 export * from './src/app/utils/icons';
+export * from './src/app/utils/enums';
+export * from './src/app/utils/components';
