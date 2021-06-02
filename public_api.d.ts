da1146b2bd9e86131d97a502291694dc05b455c3
Jessica <jessica@spoonity.com>
2021-06-02 15:47:59 -0400

    publish

--- public_api.d.ts
+++ public_api.d.ts
@@ -2,7 +2,4 @@ export * from './src/app/sp-components.module';
 export * from './src/app/sp-component/sp-components.component';
 export * from './src/app/utils/icons';
 export * from './src/app/utils/enums';
-export * from './src/app/Components/Atoms/atoms';
-export * from './src/app/Components/Molecules/molecules';
-export * from './src/app/Components/Organisms/organisms';
-export * from './src/app/Components/shared/shared';
+export * from './src/app/utils/components';
