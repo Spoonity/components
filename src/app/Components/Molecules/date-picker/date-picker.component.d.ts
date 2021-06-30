fd297bc180b2c6bfc3d8a1e2d43cc85a0c48fff1
Jessica <jessica@spoonity.com>
2021-06-30 08:10:39 -0400

    Publish sp-components

--- src/app/Components/Molecules/date-picker/date-picker.component.d.ts
+++ src/app/Components/Molecules/date-picker/date-picker.component.d.ts
@@ -1,7 +1,16 @@
 import { Renderer2 } from '@angular/core';
 import { FormFieldManager } from '../../shared/form-field.manager';
+import { NzDatePickerComponent } from 'ng-zorro-antd/date-picker';
 export declare class DatePickerComponent extends FormFieldManager {
+    /**
+     * string formatted date
+     */
     formattedDate: string;
+    /**
+     * open state for the date picker overlay
+     */
+    datePickerOpen: boolean;
+    nzDatePickerComponent: NzDatePickerComponent;
     constructor(_renderer: Renderer2);
     disabledDate: (current: Date) => boolean;
     /**
@@ -16,4 +25,13 @@ export declare class DatePickerComponent extends FormFieldManager {
      * format date to display
      */
     convertDate(event: any): void;
+    /**
+     * close the datepicker
+     */
+    closeDatePicker(): void;
+    /**
+     * listen when the datepicker overlay status changes (open/close)
+     * @param open
+     */
+    onDatePickerOpen(open: boolean): void;
 }
