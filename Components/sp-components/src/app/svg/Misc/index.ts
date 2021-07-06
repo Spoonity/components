cf65df514de1a50a8c941ed59af401af3d72c1f0
Jessica <jessica@spoonity.com>
2021-07-06 09:16:00 -0400

    add calendar icons

--- Components/sp-components/src/app/svg/Misc/index.ts
+++ Components/sp-components/src/app/svg/Misc/index.ts
@@ -1,4 +1,6 @@
 import { appCalendarIcon } from './calendar';
+import { appCalendarDateRangeIcon } from './calendar_date_range';
+import { appCalendarTodayIcon } from './calendar_today';
 import { appDownloadIcon } from './download';
 
-export const miscIcons = [appCalendarIcon, appDownloadIcon];
+export const miscIcons = [appCalendarIcon, appCalendarDateRangeIcon, appCalendarTodayIcon, appDownloadIcon];
