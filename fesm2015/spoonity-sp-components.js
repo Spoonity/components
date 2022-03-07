59f9a372bdcb97acd34338ffbd7ecaec5379ec9f
Jessica <jessica@spoonity.com>
2022-03-07 10:06:40 -0700

    Publish sp-components

--- fesm2015/spoonity-sp-components.js
+++ fesm2015/spoonity-sp-components.js
@@ -2176,7 +2176,7 @@ const fileIcons = [appUploadFileIcon];
 const hardwareIcons = [appKeyboardTabIcon, appSmartphoneIcon];
 
 const appEditIcon = {
-    data: `<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M3 17.46v3.04c0 .28.22.5.5.5h3.04c.13 0 .26-.05.35-.15L17.81 9.94l-3.75-3.75L3.15 17.1c-.1.1-.15.22-.15.36zM20.71 5.63l-2.34-2.34a.996.996 0 00-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83a.996.996 0 000-1.41z" fill="#0D0C0B"/></svg>`,
+    data: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04a.996.996 0 000-1.41l-2.34-2.34a.996.996 0 00-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>`,
     name: 'edit'
 };
 
