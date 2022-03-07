739f9eb1f1694958d66379c8cdb0968bc1e43b06
Jessica <jessica@spoonity.com>
2022-03-07 12:05:15 -0700

    Publish sp-components

--- fesm2015/spoonity-sp-components.js
+++ fesm2015/spoonity-sp-components.js
@@ -2098,7 +2098,7 @@ CardComponent.propDecorators = {
 };
 
 const appCheckCircleIcon = {
-    data: `<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4.95 8.23l-5.66 5.66a.996.996 0 01-1.41 0l-2.83-2.83a.996.996 0 111.41-1.41l2.12 2.12 4.95-4.95a.996.996 0 011.41 0c.4.39.4 1.02.01 1.41z" fill="#0D0C0B"/></svg>`,
+    data: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/></svg>`,
     name: 'check-circle'
 };
 
