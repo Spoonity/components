5631ce43daf0b04b5674bf3ac83373da0cd46900
Jessica <jessica@spoonity.com>
2022-03-29 11:02:22 -0600

    Publish sp-components

--- bundles/spoonity-sp-components.umd.js
+++ bundles/spoonity-sp-components.umd.js
@@ -2075,7 +2075,7 @@
     };
 
     var appLogoutIcon = {
-        data: "<svg fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\"><path d=\"M5 5h6c.55 0 1-.45 1-1s-.45-1-1-1H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h6c.55 0 1-.45 1-1s-.45-1-1-1H5V5z\" fill=\"#0D0C0B\"/><path d=\"M20.65 11.65l-2.79-2.79a.501.501 0 00-.86.35V11h-7c-.55 0-1 .45-1 1s.45 1 1 1h7v1.79c0 .45.54.67.85.35l2.79-2.79c.2-.19.2-.51.01-.7z\" fill=\"#0D0C0B\"/></svg>",
+        data: "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\"><path d=\"M0 0h24v24H0z\" fill=\"none\"/><path d=\"M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z\"/></svg>",
         name: 'logout'
     };
 
