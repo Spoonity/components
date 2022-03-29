cbae91875d9bb7c1f968d67155f7854f8061edc2
Jessica <jessica@spoonity.com>
2022-03-29 11:10:34 -0600

    fix all icon fills

--- Components/sp-components/src/app/svg/Navigation/more_vert.ts
+++ Components/sp-components/src/app/svg/Navigation/more_vert.ts
@@ -1,4 +1,4 @@
 export const appMoreVertIcon = {
-    data: `<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" fill="#0D0C0B"/></svg>`,
+    data: `<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/></svg>`,
     name: 'more-vert'
 };
--- Components/sp-components/src/app/svg/Toggle/radio_button_checked.ts
+++ Components/sp-components/src/app/svg/Toggle/radio_button_checked.ts
@@ -1,4 +1,4 @@
 export const appRadioButtonCheckedIcon = {
-    data: `<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z" fill="#0D0C0B"/><path d="M12 17a5 5 0 100-10 5 5 0 000 10z" fill="#0D0C0B"/></svg>`,
+    data: `<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/><path d="M12 17a5 5 0 100-10 5 5 0 000 10z"/></svg>`,
     name: 'radio-button-checked'
 };
