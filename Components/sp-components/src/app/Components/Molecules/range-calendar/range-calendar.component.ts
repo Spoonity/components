a627d903170940f288cf2d2b92869235e362b488
Jessica <jessica@spoonity.com>
2022-05-02 15:36:01 -0600

    add min/max value to range picker

--- Components/sp-components/src/app/Components/Molecules/range-calendar/range-calendar.component.ts
+++ Components/sp-components/src/app/Components/Molecules/range-calendar/range-calendar.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
+import {differenceInCalendarDays} from 'date-fns';
 
 @Component({
   selector: 'spt-range-calendar',
@@ -6,12 +7,17 @@ import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges
   styleUrls: ['../../shared/form-field.manager.less']
 })
 export class RangeCalendarComponent implements OnInit, OnChanges {
-  /** range start date **/
+  /* range start date */
   @Input() startDate: Date = new Date();
 
-  /** range end date **/
+  /* range end date */
   @Input() endDate: Date = new Date();
 
+  @Input() min: Date;
+
+  @Input() max: Date;
+
+  /* on change event */
   @Output() onChange: EventEmitter<Date[]> = new EventEmitter<Date[]>();
 
   /** date range **/
@@ -26,5 +32,21 @@ export class RangeCalendarComponent implements OnInit, OnChanges {
     if (changes.startDate || changes.endDate) {
       this.range = [this.startDate, this.endDate];
     }
+  }
+
+  disabledDate = (current: Date): boolean => {
+    if (this.min && this.max == null) {
+      return differenceInCalendarDays(current, this.min) < 0;
+    }
+
+    if (this.max && this.min == null) {
+      return differenceInCalendarDays(current, this.max) > 0;
+    }
+
+    if (this.min && this.max) {
+      return differenceInCalendarDays(current, this.min) < 0 || differenceInCalendarDays(current, this.max) > 0;
+    }
+
+    return null;
   }
 }
