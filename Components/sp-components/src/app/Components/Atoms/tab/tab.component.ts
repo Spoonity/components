9e15ca75cdc57d5d113bb8bdae69eda77f41cf31
Jessica <jessica@spoonity.com>
2022-06-08 09:24:43 -0600

    add vertical tab

--- Components/sp-components/src/app/Components/Atoms/tab/tab.component.ts
+++ Components/sp-components/src/app/Components/Atoms/tab/tab.component.ts
@@ -9,6 +9,12 @@ export class TabComponent implements OnInit {
   @Input() tabs: {name: string, disabled?: boolean, icon?: string, tooltip?: string}[];
   @Input() index = 0;
   @Output() selectedIndexChange: EventEmitter<any> = new EventEmitter<any>();
+  @Input() orientation: 'vertical' | 'horizontal' = 'horizontal';
+
+  TAB_POSITION: {[orientation: string]: 'top' | 'left'} = {
+    vertical: 'left',
+    horizontal: 'top'
+  };
 
   constructor() { }
 
