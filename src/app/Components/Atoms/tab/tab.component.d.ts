ce99e902c71ffa7670c76893527227927cf09d60
Jessica <jessica@spoonity.com>
2022-12-06 16:51:04 -0700

    Publish sp-components

--- src/app/Components/Atoms/tab/tab.component.d.ts
+++ src/app/Components/Atoms/tab/tab.component.d.ts
@@ -4,6 +4,7 @@ export declare class TabComponent implements OnInit {
         name: string;
         disabled?: boolean;
         icon?: string;
+        iconColor?: string;
         tooltip?: string;
     }[];
     index: number;
